ca5f95060ddba5f759e629de28423de480660086
Xavier Watkins <xavier.watkins@gmail.com>
2019-08-29 16:37:18 +0100

    enable all pins

--- drawers.py
+++ drawers.py
@@ -1,61 +1,66 @@
+from flask import Flask, render_template, request
+from flask_cors import CORS
+import time
+import random
+import json
 import RPi.GPIO as GPIO
 import sys
 print(sys.path)
-import json
-import random
-import time
-from flask_cors import CORS
-from flask import Flask, render_template, request
 app = Flask(__name__)
 CORS(app)
 
 GPIO.setmode(GPIO.BCM)
 
 pins = {
-#    1: {'name': 'GPIO 1', 'state': GPIO.LOW},
+    1: {'name': 'GPIO 1', 'state': GPIO.LOW},
     2: {'name': 'GPIO 2', 'state': GPIO.LOW},
     3: {'name': 'GPIO 3', 'state': GPIO.LOW},
     4: {'name': 'GPIO 4', 'state': GPIO.LOW},
-#    5: {'name': 'GPIO 5', 'state': GPIO.LOW},
-#    6: {'name': 'GPIO 6', 'state': GPIO.LOW},
-#    7: {'name': 'GPIO 7', 'state': GPIO.LOW},
-#    8: {'name': 'GPIO 8', 'state': GPIO.LOW},
-#    9: {'name': 'GPIO 9', 'state': GPIO.LOW},
-#    10: {'name': 'GPIO 10', 'state': GPIO.LOW}
+    5: {'name': 'GPIO 5', 'state': GPIO.LOW},
+    6: {'name': 'GPIO 6', 'state': GPIO.LOW},
+    7: {'name': 'GPIO 7', 'state': GPIO.LOW},
+    #    8: {'name': 'GPIO 8', 'state': GPIO.LOW},
+    #    9: {'name': 'GPIO 9', 'state': GPIO.LOW},
+    #    10: {'name': 'GPIO 10', 'state': GPIO.LOW}
 }
 
 for pin in pins:
     GPIO.setup(pin, GPIO.OUT)
     GPIO.output(pin, GPIO.LOW)
 
+
 def switchPin(pin, state):
-   print(pin,state)
-   if state == "on":
-      signal = GPIO.HIGH
-   else:
-      signal = GPIO.LOW
-   GPIO.output(pin, signal)
+    print(pin, state)
+    if state == "on":
+        signal = GPIO.HIGH
+    else:
+        signal = GPIO.LOW
+    GPIO.output(pin, signal)
+
 
 def readPins():
-   for pin in pins:
-       pins[pin]['state'] = GPIO.input(pin)
+    for pin in pins:
+        pins[pin]['state'] = GPIO.input(pin)
+
 
 def dance(duration):
     loopNumber = int(duration/0.2)
     for loop in range(loopNumber):
-       allOn()
-       randomPin = random.choice(list(pins))
-       switchPin(randomPin, 'off')
-       time.sleep(0.2)
+        allOn()
+        randomPin = random.choice(list(pins))
+        switchPin(randomPin, 'off')
+        time.sleep(0.2)
     allOff()
 
+
 def allOn():
-   for pin in pins:
-       GPIO.output(pin, GPIO.HIGH)
+    for pin in pins:
+        GPIO.output(pin, GPIO.HIGH)
+
 
 def allOff():
-   for pin in pins:
-       GPIO.output(pin, GPIO.LOW)
+    for pin in pins:
+        GPIO.output(pin, GPIO.LOW)
 
 # The function below is executed when someone requests a URL with the pin number and action in it:
 @app.route("/<changePin>/<action>")
